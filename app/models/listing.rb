993d910656ebdf36c28bfea5837ee8590163dc7e
Reggie <reggiest09@gmail.com>
2016-05-23 14:10:04 -0400

    add validations user/listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,10 @@
 class Listing < ActiveRecord::Base
   #  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+mount_uploader :picture, PictureUploader
+validates_presence_of :picture
 
-  mount_uploader :picture, PictureUploader
+  validates :name, :description, :short_description, :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+
 end
