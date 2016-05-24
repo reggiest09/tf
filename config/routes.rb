e5b737b67b636125491892665217cfffb4942d53
Reggie <reggiest09@gmail.com>
2016-05-23 22:37:51 -0400

    seller_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   root'listings#index'
-get 'pages/about'
-
+  get 'pages/about'
   get 'pages/contact'
-
+   get 'seller' => 'listings#seller'
   
 end
