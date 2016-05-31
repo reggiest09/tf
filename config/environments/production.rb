a8aa0ae3bf1a161b9dd90fe6a44f6f45a511838b
Reggie <reggiest09@gmail.com>
2016-05-31 13:46:39 -0400

    emfinal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'infinite-shore-53816.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'fundtaps.org'}
   
 end
 
